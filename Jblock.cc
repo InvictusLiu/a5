d482ee1570bc1ed9114ab869b99e3945209fc791
WINIFREDWU1103 <34110838+WINIFREDWU1103@users.noreply.github.com>
2017-11-30 17:32:54 -0500

    Update Jblock.cc

--- Jblock.cc
+++ Jblock.cc
@@ -19,11 +19,14 @@ void JBlock::down(){
 }
 
 void JBlock::counterClockwise(){
-	if ((position == 3) && coord[0].row >= 17){ return;}
-	if ((position == 2) && coord[0].col >= 10){ return;}
-	if ((position == 4) && coord[0].col <= 0){ return;}
-	for (unsigned int i =0; i < coord.size(); i++){
-		
+	vector<Coordinate> oldCoord;
+	for(auto &c : coord) {
+		oldCoord.emplace_back(c);
+	}
+	for (int i = 0; i < size; i++){
+		for (int i = 0; i < size; i++){
+			coord[i*size+j] = oldCoord[size*(size-j-1)+i];
+		}
 	}
 }
 
