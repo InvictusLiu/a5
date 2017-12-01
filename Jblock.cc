0c6e5a661e4e41a3acaf15a9cdc563ecf3362e9e
WINIFREDWU1103 <34110838+WINIFREDWU1103@users.noreply.github.com>
2017-11-30 23:11:18 -0500

    Update Jblock.cc

--- Jblock.cc
+++ Jblock.cc
@@ -1,7 +1,7 @@
 #include "Jblock.h"
 #include <vector>
 
-JBlock::JBlock(int row, int col, std::string sym, int level):Block{row, col, sym, level}{}
+JBlock::JBlock(int row, int col, std::string sym, int level):row{row}, col, sym, level}{}
 JBlock(const JBlock &other){
 }
 
