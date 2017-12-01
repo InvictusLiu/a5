3d098f081ff5b7b53fb9aaa569788589f0ddf9f8
WINIFREDWU1103 <34110838+WINIFREDWU1103@users.noreply.github.com>
2017-11-30 22:26:01 -0500

    Update Jblock.cc

--- Jblock.cc
+++ Jblock.cc
@@ -2,6 +2,8 @@
 #include <vector>
 
 JBlock::JBlock(int row, int col, std::string sym, int level):Block{row, col, sym, level}{}
+JBlock(const JBlock &other){
+}
 
 int JBlock::getLevel(){
 	return level;
