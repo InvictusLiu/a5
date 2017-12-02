a9475ccbfc2c2da0a30a30e84be2de716f4724fc
WINIFREDWU1103 <34110838+WINIFREDWU1103@users.noreply.github.com>
2017-12-02 01:01:52 -0500

    Update command.cc

--- command.cc
+++ command.cc
@@ -1,1 +1,46 @@
+#include <vector>
+#include <string>
+#include <sstring>
+#include <memory>
+#include <iostream>
 #include "command.h"
+
+using namespace std;
+
+Command::Command(int seed, string seq, bool hasGraphic, int startLevel):seed{seed},seq{seq},hasGraphic{hasGraphic},startLevel{startLevel}{
+        if (startLevel == 0){level = make_shared<Level0>(seq);}
+        else if (startLevel == 1){level = make_shared<Level1>(seed);}
+        else if (startLevel == 2){level = make_shared<Level2>(seed);}
+        else if (startLevel == 3){level = make_shared<Level3>(seed);}
+        else if (startLevel == 4){level = make_shared<Level4>(seed);}
+        else {throw invalid_argument(string "level 0-4");}
+        
+        td = make_shared<TextDisplay>();
+        if(hasGraphic){
+                gd = make_shared<GraphicsDisplay>();
+                b = make_shared<Board>(); //not sure!
+        }
+        else {b = make_shared<Board>(); //not sure!}
+}
+
+Command::interpretCommand(){
+        string s;
+        while(cin > s){
+                int start = 0;
+                int multiplier = 1;
+                string mul;
+                string cmd;
+                for (unsigned int i = 0; i<s.length(); i++){
+                        if ((s[i]>=0) && (s[i]<=9)){
+                                mul += s[i];
+                                start++;
+                        }
+                        else{break;}
+                }
+                cmd = s.substr(start);
+                istringstream iss{mul};
+                if (iss >> multiplier){}
+                
+                
+        
+}
