c2694747ca1e9130ae8131a0fb4198b811358645
WINIFREDWU1103 <34110838+WINIFREDWU1103@users.noreply.github.com>
2017-12-02 00:30:01 -0500

    Update command.h

--- command.h
+++ command.h
@@ -1,1 +1,24 @@
+#ifndef __COMMAND_H__
+#define __COMMAND_H__
 
+#include "level.h"
+#include "board.h"
+#include <memory>
+
+class Command: public Observer{
+        int seed = 0;
+        string cmd;
+        bool hasGraphic;
+        int startLevel = 0;
+        std::shared_ptr<Level> lvl;
+        std::shared_ptr<Observer> td;
+        std::shared_ptr<Observer> gd;
+        std::shared_ptr<Board> b;
+        std::vector<string> cmdCollect{left,right,down,drop,levelup,leveldown,clockwise,counterclockwise,random,norandom,restart,hint,sequence};
+        
+        public:
+        Command(int seed, string seq, bool hasGraphic, int startLevel){};
+        interpretCommand(); //notify observers
+};
+
+#endif
