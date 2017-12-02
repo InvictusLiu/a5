3bbb453eac9e700ef02f785cf4536d2a5fa8c39d
miumiumiuu <33967652+miumiumiuu@users.noreply.github.com>
2017-12-02 16:14:54 -0500

    Update level2.cc

--- level2.cc
+++ level2.cc
@@ -24,25 +24,25 @@ std::shared_ptr<Block> createBlock() {
 	srand(seed);
 	int r = rand()%7;
 	if (r == 0){
-		return make_shared<IBlock>(lvl);
+		return make_shared<IBlock>(2);
 	}
 	else if (r == 1){
-		return make_shared<JBlock>(lvl);
+		return make_shared<JBlock>(2);
 	}
 	else if (r == 2){
-		return make_shared<LBlock>(lvl);
+		return make_shared<LBlock>(2);
 	}
 	else if (r == 3){
-		return make_shared<TBlock>(lvl);
+		return make_shared<TBlock>(2);
 	}
 	else if (r == 4){
-		return make_shared<OBlock>(lvl);
+		return make_shared<OBlock>(2);
 	}
 	else if (r == 5){
-		return make_shared<ZBlock>(lvl);
+		return make_shared<ZBlock>(2);
 	} 
 	else { //r == 6
-		return make_shared<SBlock>(lvl);
+		return make_shared<SBlock>(2);
 	}
 }
 
