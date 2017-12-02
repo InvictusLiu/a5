e8291eb9a19597669a9759a0710a7c28df2ab3ea
miumiumiuu <33967652+miumiumiuu@users.noreply.github.com>
2017-12-02 16:15:40 -0500

    Update level3.cc

--- level3.cc
+++ level3.cc
@@ -22,27 +22,27 @@ Level3::int getLevel(){
 Level3::shared_ptr<Block> createBlock(){
 	srand(seed);
 	int r = srand() % 9;
-	if ((r == 0)&&(r == 1)){
-		return make_shared<SBlock>(lvl);
+	if ((r == 0)||(r == 1)){
+		return make_shared<SBlock>(3);
 
 	}
-	else if ((r == 2)&&(r == 3)){
-		return make_shared<ZBlock>(lvl);
+	else if ((r == 2)||(r == 3)){
+		return make_shared<ZBlock>(3);
 	}
 	else if (r == 4) {
-		return make_shared<JBlock>(lvl);
+		return make_shared<JBlock>(3);
 	}
 	else if (r == 5) {
-		return make_shared<LBlock>(lvl);
+		return make_shared<LBlock>(3);
 	}
 	else if (r == 6) {
-		return make_shared<OBlock>(lvl);
+		return make_shared<OBlock>(3);
 	}
 	else if (r == 7) {
-		return make_shared<TBlock>(lvl);
+		return make_shared<TBlock>(3);
 	}
 	else {
-		return make_shared<IBlock>(lvl);
+		return make_shared<IBlock>(3);
 	}
 }
 
