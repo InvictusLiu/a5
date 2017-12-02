f446abd43db3dcbd82f3496605657ec71b1dc22d
miumiumiuu <33967652+miumiumiuu@users.noreply.github.com>
2017-12-01 23:35:31 -0500

    Update level2.h

--- level2.h
+++ level2.h
@@ -8,7 +8,7 @@ class Level2: public Level{
 	unsigned int seed;
 
 public:
-	Level2(int lvl, int seed = 0);
+	Level2(int seed = 0);
 	int getLevel() override;
 	std::shared_ptr<Block> createBlock() override;
 	~Level2();
